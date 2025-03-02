0a2efcd43eaace5130790fa063a4b33ae658d607
chandan msi-wsl <chandanyuva1511@gmail.com>
2025-03-02 20:25:39 +0530

    added terminal mode esc keymap
    and also replacements for trailing spaces to '.' , nbsp to '_'

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -91,11 +91,19 @@ vim.api.nvim_create_autocmd('TextYankPost', {
 vim.opt.hlsearch = true
 vim.keymap.set('n', 'Esc', '<cmd>nohlsearch<CR>')
 
+-- Preview substitutions live, as you type!
+vim.opt.inccommand = 'split'
+
+-- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
+-- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
+-- is not what someone will guess without a bit more experience.
+--
+-- NOTE: This won't work in all terminal emulators/tmux/etc. Try your own mapping
+-- or just use <C-\><C-n> to exit terminal mode
+vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
+
 -- Sets how neovim will display certain whitespace characters in the editor.
 --  See `:help 'list'`
 --  and `:help 'listchars'`
 vim.opt.list = true
-vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
-
--- Preview substitutions live, as you type!
-vim.opt.inccommand = 'split'
+vim.opt.listchars = { trail = '·', nbsp = '␣' } -- tab = '» '
