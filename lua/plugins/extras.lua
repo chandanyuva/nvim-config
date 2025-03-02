a6c8c71e9455cf2cc6e0a88aa980027beda7ed91
chandan msi-wsl <chandanyuva1511@gmail.com>
2025-03-02 16:42:52 +0530

    added gitsigns.nvim

--- lua/plugins/extras.lua
+++ lua/plugins/extras.lua
@@ -22,4 +22,16 @@ return {
             require('colorizer').setup()
         end,
     },
+    { -- Adds git related signs to the gutter, as well as utilities for managing changes
+        'lewis6991/gitsigns.nvim',
+        opts = {
+            signs = {
+                add = { text = '+' },
+                change = { text = '~' },
+                delete = { text = '_' },
+                topdelete = { text = '‾' },
+                changedelete = { text = '~' },
+            },
+        },
+    },
 }
