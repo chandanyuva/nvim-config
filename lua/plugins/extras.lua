0de0507be9cdddc144ce6d343f8cc76a8a36c6a2
chandan msi-wsl <chandanyuva1511@gmail.com>
2025-03-02 16:52:08 +0530

    added indent-blanklines and mini.ai, mini.comment

--- lua/plugins/extras.lua
+++ lua/plugins/extras.lua
@@ -34,4 +34,11 @@ return {
             },
         },
     },
+    { -- Add indentation guides even on blank lines
+        'lukas-reineke/indent-blankline.nvim',
+        -- Enable `lukas-reineke/indent-blankline.nvim`
+        -- See `:help ibl`
+        main = 'ibl',
+        opts = {},
+    },
 }
